6f34a6342fd6dee13c523196070236afc864951b
dthayer <dthayer@bwschools.net>
2025-03-04 13:03:11 -0500

    commit

---
+++ main.py
@@ -0,0 +1,46 @@
+import turtle
+
+screen = turtle.Screen()
+screen.screensize(500,500)
+screen.bgcolor('yellow')
+
+t = turtle.Turtle()
+
+t.setheading(135)
+t.pencolor('cyan')
+t.fillcolor('cyan')
+t.begin_fill()
+t.forward(100)
+t.left(90)
+t.forward(100)
+t.left(90)
+t.forward(100)
+t.left(90)
+t.forward(100)
+t.left(90)
+t.end_fill()
+
+t.penup()
+t.goto(0,0)
+t.pendown()
+
+t.setheading(45)
+t.pencolor('brown')
+t.fillcolor('brown')
+t.begin_fill()
+t.forward(100)
+t.right(90)
+t.forward(100)
+t.right(90)
+t.forward(100)
+t.right(90)
+t.forward(100)
+t.right(90)
+t.end_fill()
+
+t.hideturtle()
+
+
+
+
+turtle.done()
